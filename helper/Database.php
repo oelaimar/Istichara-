5a2c00b7313e2a639eccdfab80dff5478d230817
safaacht <safaachtaoui@gmail.com>
2026-01-16 01:34:32 +0100

    connection avec database en implementant dependency invertion

---
+++ helper/Database.php
@@ -0,0 +1,32 @@
+<?php
+namespace helper;
+use interfaces\DatabaseInterface;
+use PDO;
+
+class Database implements DatabaseInterface{
+    private static string $host="localhost";
+    private static string $db_name="istichara";
+    private static string $user="root";
+    private static string $mdp="";
+    private static int $port=3307;
+
+    private static  $conn=null;
+
+    public static function getConnection()
+    {
+        if(self::$conn===null){
+            $dsn='mysql:host=' . self::$host . ';dbname=' . self::$db_name . ';port=' . self::$port . ';charset=utf8';
+            self::$conn= new PDO($dsn,self::$user,self::$mdp,[
+                PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE=> PDO::FETCH_ASSOC
+
+            ]);
+            
+        }
+      
+        return self::$conn;
+
+        
+    }
+}
+
---
+++ reporitories/BaseRepo.php
@@ -0,0 +1,64 @@
+<?php
+namespace repositories;
+use helper\Database;
+use PDO;
+
+abstract class BaseRepo{
+    private string $table;
+    private PDO $conn;
+
+    public function initCrud()
+    {
+        $this->conn=Database::getConnection();
+    }
+
+    public function __construct()
+    {
+        $this->initCrud();
+    }
+    // =====CREATE=====
+    public function create(array $data):bool
+    {
+        $columns=implode(",",array_keys($data));
+        $placeholders=":" . implode(",:",array_keys($data));
+
+        $stmt=$this->conn->prepare("INSET INTO {$this->table}($columns) VALUES($placeholders)");
+        return $stmt->execute($data);
+    }
+
+
+    // =====AFFICHAGE=====
+    public function affichage():array
+    {
+       $stmt=$this->conn->prepare("SELECT * FROM {$this->table}");
+       $result=$stmt->fetchALL();
+       return $result;
+    }
+
+
+    // ======DELETE=====
+    public function delete(int $id):bool
+    {
+        $stmt=$this->conn->prepare("DELETE FROM {$this->table} WHERE id=:id");
+        $result=$stmt->execute(["id"=>$id]);
+        return $result;
+    }
+
+
+    // ======UPDATE=====
+    public function update(int $id, array $data):bool
+    {
+        $fields="";
+        
+        foreach($data as $key=>$value){
+            $fields.= "$key=:$key,";
+            $fields=rtrim($fields,",");
+        }
+        $data["id"]=$id;
+
+        
+        $stmt=$this->conn->prepare("UPDATE {$this->table} SET $fields WHERE id=:id");
+        $result=$stmt->execute($data);
+        return $result;
+    }
+}
