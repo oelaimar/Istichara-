ed8078432e8a7d1df80166468d7b148aba52e0ce
safaacht <safaachtaoui@gmail.com>
2026-01-18 20:27:21 +0100

    ajout du fct getVilleNames

--- repositories/VilleRepo.php
+++ repositories/VilleRepo.php
@@ -2,7 +2,14 @@
 namespace repositories;
 use repositories\BaseRepo;
 use models\Ville;
+use helper\Database;
+use PDO;
 
 class VilleRepo extends BaseRepo{
     public string $table="city";
+
+    public function getVilleNames() {
+        $stmt = $this->conn->query("SELECT id, name FROM city");
+        return $stmt->fetchAll(PDO::FETCH_KEY_PAIR);
+    }
 }
