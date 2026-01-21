a2167a2e7620a8a925e138b48ccf5e1146ce2720
safaacht <safaachtaoui@gmail.com>
2026-01-21 10:01:14 +0100

    switching to postgres

--- helper/Database.php
+++ helper/Database.php
@@ -4,18 +4,17 @@ use interfaces\DatabaseInterface;
 use PDO;
 
 class Database implements DatabaseInterface{
-    private static string $host="localhost";
-    private static string $db_name="istichara";
-    private static string $user="root";
-    private static string $mdp="";
-    private static int $port=3307;
+    private static string $host="ep-cold-mountain-ah25jfw1-pooler.c-3.us-east-1.aws.neon.tech";
+    private static string $db_name="Istichara";
+    private static string $user="neondb_owner";
+    private static string $mdp="npg_umhYVWbnKC90";
 
     private static  $conn=null;
 
     public static function getConnection()
     {
         if(self::$conn===null){
-            $dsn='mysql:host=' . self::$host . ';dbname=' . self::$db_name . ';port=' . self::$port . ';charset=utf8';
+            $dsn='pgsql:host=' . self::$host . ';dbname=' . self::$db_name . ';port=' . ';sslmode=require';
             self::$conn= new PDO($dsn,self::$user,self::$mdp,[
                 PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE=> PDO::FETCH_ASSOC
