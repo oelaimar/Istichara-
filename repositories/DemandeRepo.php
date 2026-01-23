77f411b6713f94f6905db20a68b38819039d734f
safaacht <safaachtaoui@gmail.com>
2026-01-23 13:24:19 +0100

    crud static DemandeRepo

--- repositories/DemandeRepo.php
+++ repositories/DemandeRepo.php
@@ -1,10 +1,52 @@
 <?php
 namespace repositories;
-use repositories\BaseRepo;
+use models\Demande;
+use helper\Database;
 use PDO;
 
-class DemandeRepo extends BaseRepo {
-    protected string $table = "demande";
+class DemandeRepo  {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = Database::getConnection();
+    }
+
+    public function createAvocat(Demande $avocat){
+        $sql = 'INSERT INTO "demande" (name, email, phone, years_of_experiences, hourly_rate, specialization, consultation_online, city_id, document, status)
+            VALUES(:name, :email, :phone, :years_of_experiences, :hourly_rate, :specialization, :consultation_online, :city_id, :document, :status) RETURNING id';
+        $stmt = $this->conn->prepare($sql);
+        $result = $stmt->execute([
+            ':name' => $avocat->getName(),
+            ':email' => $avocat->getEmail(),
+            ':phone' => $avocat->getPhone(),
+            ':years_of_experiences' => $avocat->getExpYears(),
+            ':hourly_rate' => $avocat->getHourlyRate(),
+            ':specialization' => $avocat->getspecialization(),
+            ':consultation_online' => $avocat->isConsultationOnline(),
+            ':city_id' => $avocat->getCityId(),
+            ':document' => $avocat->getDocument(),
+            ':status' => 'pending', 
+        ]);
+        return $result;
+    }
+
+    public function createHuissier(Demande $huissier){
+        $sql = 'INSERT INTO "demande" (name, email, phone, years_of_experiences, hourly_rate, type, city_id, document, status)
+            VALUES(:name, :email, :phone, :years_of_experiences, :hourly_rate, :type, :city_id, :document, :status) RETURNING id';
+        $stmt = $this->conn->prepare($sql);
+        $result = $stmt->execute([
+            ':name' => $huissier->getName(),
+            ':email' => $huissier->getEmail(),
+            ':phone' => $huissier->getPhone(),
+            ':years_of_experiences' => $huissier->getExpYears(),
+            ':hourly_rate' => $huissier->getHourlyRate(),
+            ':type' => $huissier->getType(),
+            ':city_id' => $huissier->getCityId(),
+            ':document' => $huissier->getDocument(),
+            ':status' => 'pending',
+        ]);
+        return $result;
+    }
 
     public function getPending() {
         $stmt = $this->conn->prepare("SELECT * FROM demande WHERE status = 'pending'");
