aa72504fec0adecf4e4a7f42c967a68c24712164
safaacht <safaachtaoui@gmail.com>
2026-01-23 13:23:11 +0100

    modif du structure de code

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -18,49 +18,13 @@ class AdminController {
     public function accept() {
         if(isset($_POST['id'])) {
             $id = $_POST['id'];
-            $demandeRepo = new DemandeRepo();
-            $conn = Database::getConnection();
-            $conn->beginTransaction();
-
-            try {
-                $demande = $demandeRepo->findById($id);
-                if(!$demande) throw new \Exception("Demande not found");
-
-                $demandeRepo->updateStatus($id, 'accepted');
-
-                $data = [
-                    'name' => $demande['name'],
-                    'email' => $demande['email'],
-                    'phone' => $demande['phone'],
-                    'years_of_experiences' => $demande['years_of_experiences'],
-                    'hourly_rate' => $demande['hourly_rate'],
-                    'specialization'=>$demande['specialization'],
-                    'type'=>$demande['type'],
-                    'city_id' => $demande['city_id'],
-                    'consultation_online' => $demande['consultation_online'] ?? null
-                ];
-
-                $repo = null;
-                if (!empty($demande['specialization'])) {
-                    // an avocat
-                    $data['specialization'] = $demande['specialization'];
-                    $repo = new AvocatRepo();
-                } elseif (!empty($demande['type'])) {
-                    // an hussier
-                    $data['type'] = $demande['type'];
-                    $repo = new HussierRepo();
-                } else {
-                    throw new \Exception("Unknown role from data");
-                }
-                
-                if($repo) {
-                    $repo->create($data);
-                }
-                
-                $conn->commit();
-                
-            } catch (\Exception $e) {
-                $conn->rollBack();
+            
+            $validerCompte = new \services\ValiderCompte();
+            $result = $validerCompte->validerProfessionnel($id);
+            
+            if ($result !== true) {
+                // Handle error if needed, for now just redirect or echo error (could be improved)
+                // echo "Error: " . $result; 
             }
             
             header('Location: index.php?controller=admin&action=validationCompte');
